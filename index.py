5e094b42413fbcb0f5462e97a12d8e5309e10619
SB Developer <97078401+SBDeveloper10@users.noreply.github.com>
2022-12-16 09:28:10 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,85 @@
+import os
+from io import BytesIO
+from queue import Queue
+import requests
+from flask import Flask, request
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
+from movies_scraper import search_movies, get_movie
+
+
+TOKEN = os.getenv("TOKEN")
+URL = os.getenv("URL")
+bot = Bot(TOKEN)
+
+
+def welcome(update, context) -> None:
+    update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
+                              f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
+    update.message.reply_text("👇 Enter Movie Name 👇")
+
+
+def find_movie(update, context):
+    search_results = update.message.reply_text("Processing...")
+    query = update.message.text
+    movies_list = search_movies(query)
+    if movies_list:
+        keyboards = []
+        for movie in movies_list:
+            keyboard = InlineKeyboardButton(movie["title"], callback_data=movie["id"])
+            keyboards.append([keyboard])
+        reply_markup = InlineKeyboardMarkup(keyboards)
+        search_results.edit_text('Search Results...', reply_markup=reply_markup)
+    else:
+        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
+
+
+def movie_result(update, context) -> None:
+    query = update.callback_query
+    s = get_movie(query.data)
+    response = requests.get(s["img"])
+    img = BytesIO(response.content)
+    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
+    link = ""
+    links = s["links"]
+    for i in links:
+        link += "🎬" + i + "\n" + links[i] + "\n\n"
+    caption = f"⚡ Fast Download Links :-\n\n{link}"
+    if len(caption) > 4095:
+        for x in range(0, len(caption), 4095):
+            query.message.reply_text(text=caption[x:x+4095])
+    else:
+        query.message.reply_text(text=caption)
+
+
+def setup():
+    update_queue = Queue()
+    dispatcher = Dispatcher(bot, update_queue, use_context=True)
+    dispatcher.add_handler(CommandHandler('start', welcome))
+    dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
+    dispatcher.add_handler(CallbackQueryHandler(movie_result))
+    return dispatcher
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+
+@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
+def respond():
+    update = Update.de_json(request.get_json(force=True), bot)
+    setup().process_update(update)
+    return 'ok'
+
+
+@app.route('/setwebhook', methods=['GET', 'POST'])
+def set_webhook():
+    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
+    if s:
+        return "webhook setup ok"
+    else:
+        return "webhook setup failed"
---
+++ movies_scraper.py
@@ -0,0 +1,41 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+url_list = {}
+api_key = "ENTER YOUR API KEY HERE"
+
+
+def search_movies(query):
+    movies_list = []
+    movies_details = {}
+    website = BeautifulSoup(requests.get(f"https://185.53.88.104/?s={query.replace(' ', '+')}").text, "html.parser")
+    movies = website.find_all("a", {'class': 'ml-mask jt'})
+    for movie in movies:
+        if movie:
+            movies_details["id"] = f"link{movies.index(movie)}"
+            movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
+            url_list[movies_details["id"]] = movie['href']
+        movies_list.append(movies_details)
+        movies_details = {}
+    return movies_list
+
+
+def get_movie(query):
+    movie_details = {}
+    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
+    if movie_page_link:
+        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
+        movie_details["title"] = title
+        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
+        movie_details["img"] = img
+        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
+        final_links = {}
+        for i in links:
+            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+            response = requests.get(url)
+            link = response.json()
+            final_links[f"{i.text}"] = link['shortenedUrl']
+        movie_details["links"] = final_links
+    return movie_details
+
