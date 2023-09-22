ab68d0d9c45968ff2d5f689bc28ae73d61444123
SB Developer <97078401+SBDeveloper10@users.noreply.github.com>
2023-09-22 17:29:24 -0500

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -9,7 +9,7 @@ api_key = "ENTER YOUR API KEY HERE"
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://185.53.88.104/?s={query.replace(' ', '+')}").text, "html.parser")
+    website = BeautifulSoup(requests.get(f"https://mkvcinemas.skin/?s={query.replace(' ', '+')}").text, "html.parser")
     movies = website.find_all("a", {'class': 'ml-mask jt'})
     for movie in movies:
         if movie:
